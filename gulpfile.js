17a07c565e2c3fd91120336eb51443f3a6e6da70
andrei <andreilimabarbosa@outlook.com>
2025-09-29 18:41:16 -0300

    feat: Adiciona as imagens ao  projeto. Instala o plugin imagemin e configura o Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+function images(){
+    return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
 
 function styles(){
     return gulp.src('src/styles/*.scss')
@@ -7,7 +14,7 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function (){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
