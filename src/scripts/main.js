8985d37289d8c23d553310291e66a6b57a4c720e
andrei <andreilimabarbosa@outlook.com>
2025-10-07 19:58:57 -0300

    feat(função Esconde): Adiciona a class --is-hidden aos elementos do header que serão ocultados quando ter o scroll da pag.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,21 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            escondeElementosHeader();
+        }else{
+            exibeElementosHeader();
+        }
+    })
+
+     //Laço para section shows, abas de programações
+
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,12 +28,22 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
-    //Laço para section FAQ
+    //Laço para section FAQ, accordions
 
     for(let i = 0; i < questions.length; i++){
         questions[i].addEventListener('click', abreFechaResposta);
     }
 })
+
+function escondeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
